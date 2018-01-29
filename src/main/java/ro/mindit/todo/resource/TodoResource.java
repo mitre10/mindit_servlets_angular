608ec5e2a4c17ebe95bf1078b95d5a410264f4e5
Cristian Mitre <cristian.mitre@mindit.io>
2018-01-29 19:39:10 +0200

    added doDelete, doPost, doPut methods in Servlet

--- src/main/java/ro/mindit/todo/resource/TodoResource.java
+++ src/main/java/ro/mindit/todo/resource/TodoResource.java
@@ -9,6 +9,7 @@ import ro.mindit.todo.dao.TodoDao;
 import ro.mindit.todo.model.Todo;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class TodoResource extends HttpServlet {
 
@@ -27,12 +29,59 @@ public class TodoResource extends HttpServlet {
     }
 
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String json = "";
+        ObjectMapper obj = new ObjectMapper();
+        ServletInputStream reqInputStream = req.getInputStream();
+        Scanner sc = new Scanner(reqInputStream, "UTF-8").useDelimiter("\\A");
+        json = sc.next();
+        Todo t = obj.readValue(json, Todo.class);
+        try {
+            todoDao.connect();
+            todoDao.addTodo(t);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String json = "";
+        ObjectMapper obj = new ObjectMapper();
+        ServletInputStream reqInputStream = req.getInputStream();
+        Scanner sc = new Scanner(reqInputStream, "UTF-8").useDelimiter("\\A");
+        json = sc.next();
+        Todo t = obj.readValue(json, Todo.class);
+        try {
+            todoDao.connect();
+            todoDao.updateTodo(t.getName(), t.getOwner(), t.getPriority(), t.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        try {
+            todoDao.connect();
+            if (id != null) {
+                todoDao.deleteTodo(Integer.parseInt(id));
+            }
+            todoDao.disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         // set response content type
         response.setContentType("application/json");
 
-//        String json = getTodoFromMemory(request);
+
 		String json = getTodoFromDb(request);
 
         PrintWriter out = response.getWriter();
@@ -66,21 +115,6 @@ public class TodoResource extends HttpServlet {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        return json;
-    }
-
-    private String getTodoFromMemory(HttpServletRequest request) throws JsonProcessingException {
-        String json = "";
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        String id = request.getQueryString();
-        if (id != null) {
-            Todo todo = todoDao.getTodo(1);
-            json = objectMapper.writeValueAsString(todo);
-        } else {
-            List<Todo> todos = todoDao.queryAll();
-            json = objectMapper.writeValueAsString(todos);
         }
         return json;
     }
